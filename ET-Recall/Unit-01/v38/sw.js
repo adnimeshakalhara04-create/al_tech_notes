32ecb289dba943ca92496d07b5f309feca7a2585
A.D Nimesha Kalhara <a.dnimeshakalhara04@gmail.com>
2026-09-01 23:37:59 +0530

    Add v38 service worker

---
+++ ET-Recall/Unit-01/v38/sw.js
@@ -0,0 +1,5 @@
+const CACHE='et-recall-u1-v38-pwa';
+const CORE=['/','/manifest.webmanifest','/icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('/',c));return r}).catch(()=>caches.match('/')));return}if(new URL(e.request.url).origin===self.location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))}});
